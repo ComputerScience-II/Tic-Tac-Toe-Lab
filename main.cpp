1c6c6fd5f04a6cd3c29f2fd815e068430430f982
AdityaP <pathak.aditya.in@gmail.com>
2025-09-21 16:41:22 -0500

    Made the initial grid so it prints out appropriately

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+int main() {
+
+char selections[] = {'1',2,'3','4','5','6','7','8','9'};
+
+char turn = 'X';
+
+int choice;
+
+bool end = false;
+
+
+while(!end) {
+
+    for( int i = 0; i < 9; i++) {
+
+        cout << selections[i];
+
+       if (i % 3 != 2){
+
+        cout << " | ";
+
+       }
+    else if (i != 8) {
+    
+          cout << "\n---------\n";
+    
+    }
+    else {
+
+        cout << "\n";
+
+    }
+
+}
+
+cout << "Player " << turn << " , enter a digit: ";
+
+cin >> choice;
+
+
+if (choice < 1 || choice > 9) {
+    cout << "Please chooose a digit that falls inside the tic tac toe grid.\n";
+}
+
+if (selections[choice - 1] == 'X' || selections[choice - 1] == 'O') {
+
+    cout << "This slot has already been filled please choose another digit.\n";
+
+}
+
+selections[choice - 1] = turn;
+
+
+    return 0;
+}
+
