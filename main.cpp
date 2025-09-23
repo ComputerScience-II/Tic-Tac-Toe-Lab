a885253eccd8c9a49fa12ad2629b25e3c0ee76ed
AdityaP <pathak.aditya.in@gmail.com>
2025-09-23 08:45:34 -0500

    Finished score files logic

--- main.cpp
+++ main.cpp
@@ -12,137 +12,147 @@ int scoreTie = 0;
 
 bool checkWin() {
 
-    int winningCombinations[8][3] = {{0,1,2}, {3,4,5}, {6,7,8},{0,3,6}, {1,4,7}, {2,5,8},{0,4,8}, {2,4,6}};
+    int winningCombinations[8][3] = {
+
+        {0,1,2}, {3,4,5}, {6,7,8},
+        {0,3,6}, {1,4,7}, {2,5,8},
+        {0,4,8}, {2,4,6}
+
+    };
 
     for (int i = 0; i < 8; i++) {
 
-        if (selections[winningCombinations[i][0]] == turn && selections[winningCombinations[i][1]] == turn && selections[winningCombinations[i][2]] == turn) {
-
+        if (selections[winningCombinations[i][0]] == turn &&
+            selections[winningCombinations[i][1]] == turn &&
+            selections[winningCombinations[i][2]] == turn) {
             return true;
         }
-
     }
-    
     return false;
 }
 
 bool checkTie() {
 
     for (int i = 0; i < 9; i++) {
-
-        if (selections[i] != 'X' && selections[i] != 'O') {
-
-            return false;
-        }
+        if (selections[i] != 'X' && selections[i] != 'O') return false;
     }
-
     return true;
 
 }
 
 void resetGame() {
 
-    for (int i = 0; i < 9; i++) {
-
-        selections[i] = '1' + i;
-
-    }
-
+    for (int i = 0; i < 9; i++) selections[i] = '1' + i;
     turn = 'X';
-
     gameEnd = false;
 
 }
-
-
 
 int main() {
 
     string playerX, playerO;
 
     cout << "Enter name for Player X: ";
-
     getline(cin, playerX);
 
     cout << "Enter name for Player O: ";
-
     getline(cin, playerO);
 
+    ifstream xfile(playerX + "--scores.txt");
+    if (xfile) xfile >> scoreX;
+    xfile.close();
 
-    ifstream inX(playerX + "-results.txt");
+    ifstream ofile(playerO + "--scores.txt");
+    if (ofile) ofile >> scoreO;
+    ofile.close();
 
-    if (inX) inX >> scoreX;
+    ifstream dfile("draws.txt");
+    if (dfile) dfile >> scoreTie;
+    dfile.close();
 
-    inX.close();
+    char continueGame = 'y';
 
-    ifstream inO(playerO + "-results.txt");
+    while (continueGame == 'y' || continueGame == 'Y') {
 
-    if (inO) inO >> scoreO;
+        resetGame();
 
-    inO.close();
+        while (!gameEnd) {
 
-    ifstream inD("draws.txt");
+            for (int i = 0; i < 9; i++) {
 
-    if (inD) inD >> scoreTie;
-    
-    inD.close();
+                cout << selections[i];
+                if (i % 3 != 2) cout << " | ";
+                else if (i != 8) cout << "\n---------\n";
+                else cout << "\n\n";
+            }
 
+            cout << "Player " << turn << ", enter a digit: ";
+            cin >> choice;
 
-    while (!gameEnd) {
+            if (cin.fail() || choice < 1 || choice > 9) {
 
-        for (int i = 0; i < 9; i++) {
+                cin.clear();
+                cin.ignore(500, '\n');
+                cout << "Invalid, enter a digit between 1-9.\n";
+                continue;
+            }
 
-            cout << selections[i];
+            if (selections[choice - 1] == 'X' || selections[choice - 1] == 'O') {
 
-            if (i % 3 != 2) cout << " | ";
+                cout << "This slot is already full!!.\n";
+                continue;
+            }
 
-            else if (i != 8) cout << "\n---------\n";
+            selections[choice - 1] = turn;
 
-            else cout << "\n";
+            if (checkWin()) {
 
+                cout << "Player " << (turn == 'X' ? playerX : playerO) << " wins!\n";
+                gameEnd = true;
+                if (turn == 'X') scoreX++;
+                else scoreO++;
+            } 
+            else if (checkTie()) {
+
+                cout << "Game has ended in a tie.\n";
+                gameEnd = true;
+                scoreTie++;
+            } 
+            else {
+
+                turn = (turn == 'X') ? 'O' : 'X';
+            }
         }
 
-        cout << "Player " << turn << ", enter a digit: ";
+        cout << "\n--- Stats ---\n";
 
-        cin >> choice;
+        cout << playerX << " (X) Wins: " << scoreX << "\n";
+        cout << playerO << " (O) Wins: " << scoreO << "\n";
+        cout << "Draws: " << scoreTie << "\n";
 
-        if(cin.fail() || choice < 1 || choice > 9) {
+        ofstream xFile(playerX + "-results.txt"); xFile << scoreX; xFile.close();
 
-            cin.clear();
-            cin.ignore(500, '\n');
-            cout << "Invalid input. Please enter a number between 1 and 9.\n";
-            continue;
+        ofstream ofile(playerO + "-results.txt"); ofile << scoreO; ofile.close();
 
+        ofstream dFile("draws.txt"); dFile << scoreTie; dFile.close();
+
+       cout << "Do you want to play another game? (y/n): ";
+
+        cin >> continueGame;
+
+        while (cin.fail() || (continueGame != 'y' && continueGame != 'Y' && continueGame != 'n' && continueGame != 'N')){
+
+            cin.clear();            
+
+            cin.ignore(500, '\n');     
+
+            cout << "Invalid input. Please enter y or n: ";
+
+            cin >> continueGame;   
         }
 
-        if (selections[choice - 1] == 'X' || selections[choice - 1] == 'O') {
-
-            cout << "This slot has already been filled. Choose another.\n";
-
-        }
-
-        selections[choice - 1] = turn;
-
-        if (checkWin()) {
-
-            cout << "Player " << turn << " wins!\n";
-            gameEnd = true;
-            cout << "Game over!\n";
-
-        } 
-
-        else if (checkTie()) {
-
-            cout << "Game has ended in a tie.\n";
-            gameEnd = true;
-            cout << "Game over\n";
-
-        } 
-
-        else {
-
-            turn = (turn == 'X') ? 'O' : 'X';
-            
-        }
     }
+
+    cout << "Thanks for playing\n";
+    return 0;
 }
